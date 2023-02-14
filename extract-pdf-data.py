9085867344d3eac00c8d0970e5d73fbb67f4fe0b
dark-teal-coder <ree.wong.career@gmail.com>
2023-02-14 16:39:05 -0600

    Trim white spaces

--- extract-pdf-data.py
+++ extract-pdf-data.py
@@ -5,22 +5,22 @@ import os
 
 
 order = {}
-ca_province_code = {"ALBERTA":"AB", 
-    "BRITISH COLUMBIA":"BC", 
-    "MANITOBA":"MB", 
-    "NEW BRUNSWICK":"NB", 
-    "NOVA SCOTIA":"NS", 
-    "NEWFOUNDLAND":"NL", 
-    "YUKON":"YK", 
-    "QUEBEC":"QC", 
-    "NUNAVUT":"NU", 
-    "ONTARIO":"ON", 
-    "PRINCE EDWARD ISLAND":"PE", 
-    "SASKATCHEWAN":"SK", 
+ca_province_code = {"ALBERTA":"AB",
+    "BRITISH COLUMBIA":"BC",
+    "MANITOBA":"MB",
+    "NEW BRUNSWICK":"NB",
+    "NOVA SCOTIA":"NS",
+    "NEWFOUNDLAND":"NL",
+    "YUKON":"YK",
+    "QUEBEC":"QC",
+    "NUNAVUT":"NU",
+    "ONTARIO":"ON",
+    "PRINCE EDWARD ISLAND":"PE",
+    "SASKATCHEWAN":"SK",
     "Québec":"QC"}
 
 
-def get_pdf_page_values(pdf, page_num): 
+def get_pdf_page_values(pdf, page_num):
     page = pdf.pages[page_num]
     page_text = page.extract_text()
     # print(page_text)
@@ -40,13 +40,13 @@ def get_pdf_page_values(pdf, page_num):
     i_order_id = page_text_list.index("Canada") + 1
     v_order_id = page_text_list[i_order_id][-19:] ## 19-digit order ID
     order.update({"Order ID": v_order_id})
-    ## SKU 
+    ## SKU
     result = [s for s in page_text_list if s.startswith("SKU")]
     v_sku = result[0][5:] ## Remove the 1st 5 characters
     v_sku = v_sku.upper()
     v_sku = v_sku.rstrip("-") ## Remove "-" in SKU
     order.update({"SKU": v_sku})
-    ## Unit Price 
+    ## Unit Price
     result = [s for s in page_text_list if s.startswith("Item subtotal")]
     i_item_sub = page_text_list.index(result[0])
     v_unit_price = float(page_text_list[i_item_sub - 1].split("CAD$")[1])
@@ -54,13 +54,13 @@ def get_pdf_page_values(pdf, page_num):
     ## Quantity
     # Should use the string before "Item subtotal" instead because there might be more than 1 item in the order
     i_quantity = page_text_list.index("Quantity  Product Details Unit price Order Totals")
-    v_quantity = int(page_text_list[i_quantity + 1].split()[0]) 
+    v_quantity = int(page_text_list[i_quantity + 1].split()[0])
     order.update({"Quantity": v_quantity})
     ## Grand Total
     result = [s for s in page_text_list if s.startswith("Grand total:")]
     v_grand_total = float(result[0][17:])
     order.update({"Grand Total (CAD$)": v_grand_total})
-    ## Address 
+    ## Address
     i_canada = page_text_list.index("Canada")
     i_before = i_custname
     i_after = i_canada
@@ -78,7 +78,7 @@ def get_pdf_page_values(pdf, page_num):
     v_province = v_address[-1].split(",")[1].strip()
     v_province = v_province.title()
     order.update({"Province": v_province})
-    
+
     return order
 
 
@@ -92,7 +92,7 @@ with pdfplumber.open(f"Amazon Manage Orders {today_date}.pdf") as pdf:
     for i in range(num_of_pages):
         order = get_pdf_page_values(pdf, i)
         print(order)
-        for value in order.values(): 
+        for value in order.values():
             print(value)
         print("-"*10)
 
