c7b3beef255fda2b93a9626233fb0002fd2db120
dark-teal-coder <ree.wong.career@gmail.com>
2023-02-17 10:43:51 -0600

    Trim white spaces in automate_gui.py

--- automate_gui.py
+++ automate_gui.py
@@ -1,6 +1,8 @@
+## Testing
 import pyautogui
- 
+
+
 ## Return width and height of the screen in pixels
 print(pyautogui.size())
-## Return the position of the mouse cursor 
+## Return the position of the mouse cursor
 print(pyautogui.position())
