3bf6893f0a64e1180abdcdae089e4e099b2d88c6
dark-teal-coder <ree.wong.career@gmail.com>
2023-02-17 16:56:13 -0600

    Add example of PyPDF2

--- extract-pdf-data.py
+++ extract-pdf-data.py
@@ -157,15 +157,15 @@ print(orders)
 # print(file_r.read())
 # file_r.close()
 
-from PyPDF2 import PdfFileWriter, PdfFileReader
+# from PyPDF2 import PdfFileWriter, PdfFileReader
 
-output_pdf = PdfFileWriter()
+# output_pdf = PdfFileWriter()
 
-with open('input.pdf', 'rb') as readfile:
-	input_pdf = PdfFileReader(readfile)
+# with open('input.pdf', 'rb') as readfile:
+# 	input_pdf = PdfFileReader(readfile)
 
-	for page in reversed(input_pdf.pages):
-		output_pdf.addPage(page)
+# 	for page in reversed(input_pdf.pages):
+# 		output_pdf.addPage(page)
 
-	with open('output.pdf', "wb") as writefile:
-		output_pdf.write(writefile)
+# 	with open('output.pdf', "wb") as writefile:
+# 		output_pdf.write(writefile)
